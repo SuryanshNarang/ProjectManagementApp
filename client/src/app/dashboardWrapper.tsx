cf3a77482ce0437dfac683e5db88f13d6c94a17c
Suryansh Narang <suryanshnarang23@gmail.com>
2024-10-20 21:45:55 +0530

    sidebar update 1

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "@app/(components)/Navbar"; // Adjust based on actual path
+import Sidebar from "@app/(components)/Sidebar";
+
 
 const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
-      sidebar
+      <Sidebar/>
       {/* main is the right side of the page. */}
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
