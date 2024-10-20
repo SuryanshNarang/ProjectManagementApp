aa5c123d8ab5b36dfdbff947c850e6046fea2d41
Suryansh Narang <suryanshnarang23@gmail.com>
2024-10-20 20:24:22 +0530

    index.tsx navbar

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "@app/(components)/Navbar"; // Adjust based on actual path
+
+const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* sidebar */}
+      sidebar
+      {/* main is the right side of the page. */}
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+        {/* this includes our right side (basically the navbar and the content ) */}
+        {/* navbar  */}
+        <Navbar />
+        {children} //this will include our content
+      </main>
+    </div>
+  );
+};
+
+export default dashboardWrapper;
